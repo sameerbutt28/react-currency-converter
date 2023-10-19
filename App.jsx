90066e08d59a900b5da3351867743a8c4fed6cbb
Sameer Ali Butt <108467884+sameerbutt28@users.noreply.github.com>
2023-10-19 19:55:37 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import Label from "./components/Label"
+
+const App = () => {
+  return (
+    <div>
+      <Label
+      label="sameer"
+      amount='22'
+      />
+    </div>
+  )
+}
+
+export default App
