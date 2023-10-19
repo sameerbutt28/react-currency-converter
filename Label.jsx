23a5a1c76409f355ce8b5fcb0b1f29064d5828e4
Sameer Ali Butt <108467884+sameerbutt28@users.noreply.github.com>
2023-10-19 19:52:02 +0500

    Add files via upload

--- Label.jsx
+++ Label.jsx
@@ -1,9 +1,33 @@
+import PropTypes from 'prop-types';
 
-
-function Label() {
+function Label({
+  label,
+  amount,
+  onAmountChange,
+  onCurrencyChange,
+  currencyOptions =[],
+  selectedCurrency = 'usd',
+  amountDisabled = false,
+  currencyDisabled = false,
+  className=''
+}) {
   return (
-    <div>Label</div>
-  )
+    <div>
+      <h1> my name is {label} and my name is {amount} ali butt </h1>
+    </div>
+  );
 }
+Label.propTypes = {
+  label: PropTypes.string,
+  amount: PropTypes.int,
+  onAmountChange:PropTypes.int,
+  onCurrencyChange:PropTypes.string,
+  currencyOptions:PropTypes.Obj,
+  selectedCurrency:PropTypes.string, 
+  amountDisabled:PropTypes.bool,
+  currencyDisabled:PropTypes.bool, 
+  className:PropTypes.string,
+  
+};
 
-export default Label
+export default Label;
